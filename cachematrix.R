490d83f7abcd1a7d76c023048b0ab4c5723e730f
ZTang813 <32170790+ZTang813@users.noreply.github.com>
2018-02-03 14:19:45 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,7 @@
 
 ## Write a short comment describing this function
 ## I changed the original example according to the prompt to calculate the inverse matrix with a new MatrixCache Class
+## This function creates our own matrix class. 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -19,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## This function enables us to calculate the inverse depending on whether the inverse is calculated in advance or not .
+## If the inverse has never been calculated, we calculate it, or else, we just return the previous calculated value.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
